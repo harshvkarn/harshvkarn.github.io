912ac828d2ef6a606eb02cc1bf77cb10472cc14a
Harshvardhan Karn <harshvkarn54@gmail.com>
2018-01-10 19:14:38 +0530

    added ajax chat phpv1

--- AJAX/send.php
+++ AJAX/send.php
@@ -1,16 +1,27 @@
 <?php
 
-                $current_data = file_get_contents('chat.json');  
+                if(file_exists('chat.json'))  
+           {  
+                $current_data = file_get_contents('chat.json'); 
                 $array_data = json_decode($current_data, true);  
                 $extra = array(  
-                     'name'         =>     $_GET['name'], 
-                     'msg'          =>     $_GET['msg'],    
-                ); 
-
+                     'name'               =>     $_GET["name"],  
+                     'msg'          =>     $_GET["msg"] 
+                );  
                 $array_data[] = $extra;  
-                $final_data = json_encode($array_data[]);  
-                echo $final_data;
-                file_put_contents('chat.json', $final_data);
+                $final_data = json_encode($array_data);  
+                if(file_put_contents('chat.json', $final_data))  
+                {  
+                     $message = "<label class='text-success'>File Appended Success fully</p>";  
+                    echo $message;
+                }  
+           }  
+           else  
+           {  
+                $error = 'JSON File not exits';  
+                echo $error;
+           }  
+      }  
 
   
 ?>
