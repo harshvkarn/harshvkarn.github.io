5eb7c67620e36296fa25269baafa7aec65e661ea
Harshvardhan Karn <harshvkarn54@gmail.com>
2018-01-10 19:05:06 +0530

    added ajax chat

---
+++ AJAX/send.php
@@ -0,0 +1,16 @@
+<?php
+
+                $current_data = file_get_contents('chat.json');  
+                $array_data = json_decode($current_data, true);  
+                $extra = array(  
+                     'name'         =>     $_GET['name'], 
+                     'msg'          =>     $_GET['msg'],    
+                ); 
+
+                $array_data[] = $extra;  
+                $final_data = json_encode($array_data[]);  
+                echo $final_data;
+                file_put_contents('chat.json', $final_data);
+
+  
+?>
